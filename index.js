24d1821abeddfbbe9b162f41993de3d3b39caaad
Niall A <40417661@live.napier.ac.uk>
2019-01-19 20:32:41 +0000

    update fixing the bug for not loading the search page
    app.get("*" .. -> app.get("/preview/*" ..

--- index.js
+++ index.js
@@ -48,7 +48,7 @@ app.post("/filesFind", (req, res) => {
         };
 
 
-        try { //start landing page compilation
+        try { //start landing page compilationrs
             var src = fs.readFileSync("./html/results.html");
             src = src.toString();
         } catch (error) {
@@ -56,7 +56,7 @@ app.post("/filesFind", (req, res) => {
         }
         var template = handlebars.compile(src);
         var data = {
-            "resultsNum": 2,
+            "resultsNum": numFound.length,
             "table": tablePass
         };
         result = template(data);
@@ -68,24 +68,7 @@ app.post("/filesFind", (req, res) => {
 
 });
 
-app.get("/", (req,res) => {
-    // https://medium.freecodecamp.org/learn-how-to-handle-authentication-with-node-using-passport-js-4a56ed18e81e
-    try { //start landing page compilation
-        var src = fs.readFileSync("./html/index.html");
-        src = src.toString();
-    } catch (error) {
-        console.log(error);
-    }
-    var template = handlebars.compile(src);
-    var data = {
-
-    };
-    result = template(data);
-
-    res.send(result) //shows the user the login page from the src file location
-})
-
-app.get("/login", (req, res) => {
+app.get("/", (req, res) => {
     try { //start landing page compilation
         var src = fs.readFileSync("./html/index.html");
         src = src.toString();
@@ -118,7 +101,7 @@ app.post("/preview", (req, res) => {
     res.send(result) //shows the user the login page from the src file location
 })
 
-app.get('*', function (req, res) {
+app.get('/preview/*', function (req, res) {
     //what to do with a url that doesnt exist!
     
     var url = req.url;
